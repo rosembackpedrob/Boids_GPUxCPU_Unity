769429fd1a55abe5e459b58fe444d1a692558002
rosembackpedrob <rosembackpedrob@gmail.com>
2024-09-15 04:59:19 -0300

    Refactored and Fixed Sequential Implementation
    - The algorithm is working now.
    - We also started adding somethings for the GPU implementation (in next commit)

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -1,93 +1,251 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+public struct BoidProperties
+{
+    //Boundaries
+    public float mapHeight;
+    public float mapWidth;
+
+    //The radiuses for testing the neighbour boids 
+    //similar function to visionRadius of the tests version
+    public float separationRadius;
+    public float alignmentRadius;
+    public float cohesionRadius;
+
+    //Weights of each Rule
+    public float separationWeight;
+    public float alignmentWeight;
+    public float cohesionWeight;
+
+    //Speeds
+    public float minSpeed;
+    public float maxSpeed;
+    public int rotationSpeed;
+}
+
+public struct BoidGPU
+    {
+        public Vector3 velocity;
+        public Vector3 position;
+        public Vector3 separationVelocity;
+        public Vector3 alignmentVelocity;
+        public Vector3 cohesionVelocity;
+
+        public static int Size //get size in memory of this struct
+        {
+            get { return sizeof(float) * 3 * 5; }
+        }
+    };
 
 public class Boid : MonoBehaviour
 {
-    [Header("Propriedades")]
-    [SerializeField] Vector3 destiny;
-    [SerializeField] float minSpeed;
-    [SerializeField] float maxSpeed;
-    [SerializeField] public float visionRadius;
-    [SerializeField] float driftRot; //max value for rotation to change the direction
-
-    [Header("Pesos")]
-    [SerializeField] float cohesion = 1f;
-    [SerializeField] float separation = 1f;
-    [SerializeField] float alignment = 1f;
-
-    [Header("Jaula")]
-    [SerializeField] float boundaryMinX = -10f;
-    [SerializeField] float boundaryMaxX = 10f;
-    [SerializeField] float boundaryMinY = -10f;
-    [SerializeField] float boundaryMaxY = 10f;
-    [SerializeField] float boundaryMinZ = -10f;
-    [SerializeField] float boundaryMaxZ = 10f;
-
-    public int numberOfNeighbours;
+    public BoidProperties boidProperties;
+    public Vector3 velocity;
     
-    public Vector3 alignmentDestiny;
-    public Vector3 separationDestiny;
-    public Vector3 cohesionDestiny;
-
-    private void Update()
+    public void StartBoid(BoidProperties boidProperties, Quaternion rotation)
     {
-        HandleBoundaries();
+        //Here we pass the values defined for it's properties 
+        //and define a start velocity Vector
+        this.boidProperties = boidProperties;
+        this.velocity = rotation * Vector3.forward * this.boidProperties.maxSpeed;
     }
-
-    public void BoidMovement()
+    
+    public void UpdateBoidCPU(List<Boid> boids) 
     {
-        this.destiny = Vector3.zero; //restart direction to follow
+        Vector3 separationVelocity = SeparationRule(boids);
+        Vector3 alignmentVelocity = AlignmentRule(boids);
+        Vector3 cohesionVelocity = CohesionRule(boids);
 
-        if(numberOfNeighbours != 0)
-        {
-            //cohesion and separation direction vectors
-            //will be constantly changing and combined for the checks
-            //while alignment will be constant
-            cohesionDestiny = cohesionDestiny / numberOfNeighbours;
-            Vector3 cohesionMovement = cohesionDestiny * cohesion;
-            this.destiny += cohesionMovement;
-
-            Vector3 separationMovement = separationDestiny * separation;
-            this.destiny += separationMovement;
-
-            Vector3 alignmentMovement = alignmentDestiny * alignment;
-            this.destiny += alignmentMovement;
-        }
-        else
-        {
-            destiny = transform.forward;
-        }
+        this.velocity +=separationVelocity;
+        this.velocity += alignmentVelocity;
+        this.velocity += cohesionVelocity;
 
         
-        float movementSpeed = Mathf.Clamp(destiny.magnitude, minSpeed, maxSpeed);
-        this.transform.position += destiny.normalized * movementSpeed * Time.deltaTime;
-
-        
-
-        
+        //then we need to apply values to the actual boid object:
+        LimitSpeed(); //Limit velocity Vector first
+        UpdatePosition();
+        UpdateRotation();
+        DrawDirectionRay();
         
     }
 
-    private void HandleBoundaries()
+    public void UpdateBoidGPU(Vector3 separationVelocity, 
+                                Vector3 alignmentVelocity, 
+                                Vector3 cohesionVelocity) 
     {
-        Vector3 pos = transform.position;
+        this.velocity +=separationVelocity;
+        this.velocity += alignmentVelocity;
+        this.velocity += cohesionVelocity;
 
-        if (pos.x < boundaryMinX || pos.x > boundaryMaxX)
+        //then we need to apply values to the actual boid object:
+        LimitSpeed(); //Limit velocity Vector first
+        UpdatePosition();
+        UpdateRotation();
+        
+    }
+
+    private Vector3 SeparationRule(List<Boid> boids)
+    {
+        int numOfNeighbours = 0;
+        Vector3 separationVelocity = Vector3.zero;
+        Vector3 boidPosition = transform.position;
+
+        foreach (Boid neighbour in boids)
         {
-            Debug.Log("Entrou");
-            destiny.x = -destiny.x;
+            if(neighbour.gameObject != this.gameObject) //skip itself
+            {
+                Vector3 neighbourPosition = neighbour.transform.position;
+                float distance = Vector3.Distance(boidPosition, neighbourPosition); //(a-b).magnitude in unity
+                
+                //checks if on radius
+                if(distance < boidProperties.separationRadius)
+                {
+                    //Vector that points from A to B is the one it needs to get away from neighbour:
+                    Vector3 separationVector = boidPosition - neighbourPosition;
+                    Vector3 separationDirection = separationVector.normalized; //mag: 1
+                    
+                    //weights the distance based on the proximity,
+                    //closer to the neighbor = separation is more forceful, and conversely...
+                    separationDirection /= distance;
+
+                    separationVelocity += separationDirection; //add to the velocity Vec
+
+                    numOfNeighbours++;
+                }
+            }
         }
-        if (pos.y < boundaryMinY || pos.y > boundaryMaxY)
+        if(numOfNeighbours > 0)
         {
-            Debug.Log("Entrou");
-            destiny.y = -destiny.y;
+            separationVelocity /= (float)numOfNeighbours; //to get the avereged velocity
+            separationVelocity *= boidProperties.separationWeight; //mult by weight
         }
-        if (pos.z < boundaryMinZ || pos.z > boundaryMaxZ)
+
+        return separationVelocity;
+    }
+
+    private Vector3 AlignmentRule(List<Boid> boids)
+    {
+        int numOfNeighbours = 0;
+        Vector3 alignmentVelocity = Vector3.zero;
+        Vector3 boidPosition = transform.position;
+
+        foreach (Boid neighbour in boids)
         {
-            Debug.Log("Entrou");
-            destiny.z = -destiny.z;
+            if(neighbour.gameObject != this.gameObject)
+            {
+                Vector3 neighbourPosition = neighbour.transform.position;
+                float distance = Vector3.Distance(boidPosition, neighbourPosition);
+
+                if(distance < boidProperties.alignmentRadius)
+                {
+                    //add the neighbour velocity to this boid alignmentVelocity
+                    alignmentVelocity += neighbour.velocity;
+                    numOfNeighbours++;
+                }
+            }
         }
+
+        if(numOfNeighbours > 0)
+        {
+            //now avarage the velocity and mult by rule weight
+            alignmentVelocity /= (float)numOfNeighbours;
+            alignmentVelocity *= boidProperties.alignmentWeight;
+        }
+
+        return alignmentVelocity;
+    }
+
+    private Vector3 CohesionRule(List<Boid> boids)
+    {
+        int numOfNeighbours = 0;
+        Vector3 neighboursCenterPosition = Vector3.zero;
+        Vector3 cohesionVelocity = Vector3.zero;
+        Vector3 boidPosition = transform.position;
+
+        foreach (Boid neighbour in boids)
+        {
+            if(neighbour.gameObject != this.gameObject)
+            {
+                Vector3 neighbourPosition = neighbour.transform.position;
+                float distance = Vector3.Distance(boidPosition, neighbourPosition);
+
+                if(distance < boidProperties.cohesionRadius)
+                {
+                    neighboursCenterPosition += neighbourPosition;
+                    numOfNeighbours++;
+                }
+            }
+        }
+
+        if(numOfNeighbours > 0)
+        {
+            neighboursCenterPosition /= (float)numOfNeighbours;
+            
+            //Vector B to A, what boid needs to run torwards neighbour
+            Vector3 cohesionVector = neighboursCenterPosition - boidPosition;
+            Vector3 cohesionDirection = cohesionVector.normalized;
+            
+            cohesionVelocity = cohesionDirection * boidProperties.cohesionWeight;
+
+        }
+
+        return cohesionVelocity;
+    }
+
+    private void UpdateRotation()
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(velocity);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 
+                                    boidProperties.rotationSpeed * Time.deltaTime);
+    }
+
+    private void UpdatePosition()
+    {
+        Vector3 newPosition = transform.position + velocity * Time.deltaTime;
+
+        //Check collisions with map bounds
+        if (newPosition.z > boidProperties.mapHeight || newPosition.z < -boidProperties.mapHeight)
+        {
+            newPosition.z = Mathf.Clamp(newPosition.z, -boidProperties.mapHeight, boidProperties.mapHeight);
+            velocity.z *= -1;
+        }
+
+        if (newPosition.x > boidProperties.mapWidth || newPosition.x < -boidProperties.mapWidth)
+        {
+            newPosition.x = Mathf.Clamp(newPosition.x, -boidProperties.mapWidth, boidProperties.mapWidth);
+            velocity.x *= -1;
+        }
+
+        this.transform.position = newPosition; //udpadate the position
+    }
+
+
+    private void LimitSpeed()
+    {
+        Vector3 direction = velocity.normalized;
+        float speed = velocity.magnitude;
+        speed = Mathf.Clamp(speed, boidProperties.minSpeed, boidProperties.maxSpeed);
+        velocity = direction * speed;
+    }
+
+    public void DrawDirectionRay()
+    {
+        Debug.DrawRay(transform.position, this.velocity, Color.red);
+    }
+
+    public BoidGPU StartBoidGPU()
+    {
+        return new BoidGPU
+        {
+            velocity = this.velocity,
+            position = this.transform.position,
+            separationVelocity = Vector3.zero,
+            alignmentVelocity = Vector3.zero,
+            cohesionVelocity = Vector3.zero,
+        };
     }
 
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -4,64 +4,147 @@ using UnityEngine;
 
 public class BoidSimulation : MonoBehaviour
 {
-    [SerializeField] GameObject boidPrefab;
-    [SerializeField] int boidsNumber = 10;
-    [SerializeField] Boid[] boids;
+    [Header("Map Boundaries")]
+    public float mapHeight = 10;
+    public float mapWidth = 10;
 
-    void Start()
+    [Header("Boid Properties")]
+    public int numOfBoids = 10;
+    public GameObject boidPrefab;
+
+    public enum OPTION
     {
-        boids = new Boid[boidsNumber];
+        CPUSEQUENTIAL,
+        GPUCOMPUTESHADER,
+    }
 
-        // Initialize all boids and add them to the array
-        for (int i = 0; i < boidsNumber; i++)
+    public OPTION algorithmOption = OPTION.CPUSEQUENTIAL;
+    public List<Boid> boids;
+
+    [Header("Vision Radiuses")]
+    public float separationRadius = 1;
+    public float alignmentRadius = 2;
+    public float cohesionRadius = 3;
+
+    [Header("Rules Weights")]
+    public float separationWeight = 0.1f;
+    public float alignmentWeight = 0.1f;
+    public float cohesionWeight = 0.1f;
+
+    [Header("Boid Speeds")]
+    public float minSpeed = 0.3f;
+    public float maxSpeed = 2f;
+    public int rotationSpeed = 8;
+    
+    public ComputeShader computeShader;
+
+
+    private void Start() 
+    {
+        SpawnBoids();
+    }
+
+    private void Update() 
+    {
+        switch (algorithmOption)
         {
-            Vector3 startPos = this.transform.position 
-                                + new Vector3(0.1f, 0.1f, 0.1f) * i;
-
-            GameObject boid = Instantiate(boidPrefab, startPos, boidPrefab.transform.rotation);
-            boids[i] = boid.GetComponent<Boid>();
-
-            // Randomize start rotation
-            boid.transform.Rotate(0, Random.Range(0, 360), 0);
+            case OPTION.CPUSEQUENTIAL:
+                BoidsSequentialSimulation();
+                UpdateProperties();
+            break;
+            
+            case OPTION.GPUCOMPUTESHADER:
+                BoidsGPUSimulation();
+                UpdateProperties();
+            break;
         }
     }
 
-    void Update()
+    public void SpawnBoids()
     {
-        // Reset and calculate for each boid
-        for (int i = 0; i < boidsNumber; i++)
+        boids = new List<Boid>();
+
+        for (int i = 0; i < numOfBoids; i++)
         {
-            Boid currentBoid = boids[i];
-            currentBoid.numberOfNeighbours = 0;
-            currentBoid.cohesionDestiny = Vector3.zero;
-            currentBoid.separationDestiny = Vector3.zero;
-            currentBoid.alignmentDestiny = Vector3.zero;
+            float xPos = Random.Range(-mapWidth +3, mapWidth -3);
+            float zPos = Random.Range(-mapHeight + 3, mapHeight -3);;
+            Vector3 startPos = new Vector3(xPos, 0f, zPos);
+            Quaternion startRot = Quaternion.Euler(0, Random.Range(0, 360), 0); //rand rotation to start
 
-            for (int neighbour = 0; neighbour < boidsNumber; neighbour++)
+            GameObject boidObject = Instantiate(boidPrefab, startPos, startRot);
+            
+            Boid boidComponent = boidObject.GetComponent<Boid>();
+            BoidProperties boidProperties = InitializeProperties();
+            boidComponent.StartBoid(boidProperties, startRot);
+            
+            boids.Add(boidComponent);
+        }
+    }
+
+    private BoidProperties InitializeProperties()
+    {
+        BoidProperties boidProperties = new BoidProperties
+        {
+
+            mapHeight = this.mapHeight,
+            mapWidth = this.mapWidth,
+
+            separationRadius = this.separationRadius,
+            alignmentRadius = this.alignmentRadius,
+            cohesionRadius = this.cohesionRadius,
+   
+            separationWeight = this.separationWeight,
+            alignmentWeight = this.alignmentWeight,
+            cohesionWeight = this.cohesionWeight,
+
+            minSpeed = this.minSpeed,
+            maxSpeed = this.maxSpeed,
+            rotationSpeed = this.rotationSpeed,
+        };
+        return boidProperties;
+    }
+
+
+    //this was added so the changes done in the inspector 
+    //are executed during update for debugging
+    private void UpdateProperties()
+    {
+        BoidProperties updatedBoidProperties  = InitializeProperties();;
+
+        foreach (Boid b in boids)
+        {
+            if (b == null)
             {
-                if (neighbour != i)
-                {
-                    Boid neighbourBoid = boids[neighbour];
-                    Vector3 boidNeighbourPos = neighbourBoid.transform.position;
-                    Vector3 boidPos = currentBoid.transform.position;
-
-                    float distance = Vector3.Distance(boidNeighbourPos, boidPos);
-
-                    if (currentBoid.visionRadius >= distance)
-                    {
-                        currentBoid.numberOfNeighbours++;
-                        currentBoid.alignmentDestiny += neighbourBoid.transform.forward;
-                        currentBoid.cohesionDestiny += neighbourBoid.transform.position;
-
-                        if (currentBoid.visionRadius / 2 >= distance)
-                        {
-                            currentBoid.separationDestiny -= (boidNeighbourPos - boidPos).normalized;
-                        }
-                    }
-                }
+                continue;
             }
 
-            currentBoid.BoidMovement();
+            b.boidProperties = updatedBoidProperties;
         }
     }
+
+    private void BoidsSequentialSimulation()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        for (int i = 0; i < boids.Count; i++)
+        {
+            boids[i].UpdateBoidCPU(boids);
+        }
+        
+        float endTime = Time.realtimeSinceStartup;
+        float seqTime = endTime - startTime;
+        int FPS = (int)(1 / Time.deltaTime);
+
+        /* if(Time.realtimeSinceStartup >= 9f)
+        {
+            Debug.Log($"Tempo de Execução do Sequencial:\n {seqTime}\n" +
+                    $"Frametime: {Time.deltaTime} & FPS: {FPS}");
+        } */
+            
+    }
+
+    private void BoidsGPUSimulation()
+    {
+        
+    }
+
 }
